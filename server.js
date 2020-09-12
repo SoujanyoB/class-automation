01a8dc47f6af31f324f331995f1727d4b6e5640e
Soujanyo Biswas <biswassoujanyo@gmail.com>
2020-09-12 15:32:26 +0530

    changed the looks of the page and made it fairly responsive

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const CronJob = require('cron').CronJob;
 const ejs = require('ejs');
 const path = require('path');
 const bodyParser = require('body-parser');
-const _= require('lodash')
-const {mongoose} = require('./server/db/mongoose');
+const _ = require('lodash')
+const { mongoose } = require('./server/db/mongoose');
 
-const {User} = require('./server/models/user');        //Requiring User model
-var {authenticate}=require('./server/middleware/authenticate');
+const { User } = require('./server/models/user'); //Requiring User model
+var { authenticate } = require('./server/middleware/authenticate');
 // const fs = require('fs');
 
 // const port = process.env || 3000;
@@ -20,7 +20,7 @@ app.set('view-engine', ejs);
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(path.join(__dirname, 'public/fonts')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/login', (req, res) => {
     res.render('login.ejs');
@@ -32,16 +32,16 @@ app.get('/register', (req, res) => {
 });
 
 //Registering users
-app.post('/register',(req,res)=>{
+app.post('/register', (req, res) => {
 
-    var body= _.pick(req.body,['email','password']);                    //Picking email and password from the request
+    var body = _.pick(req.body, ['email', 'password']); //Picking email and password from the request
 
-    var user= new User(body);                                           //New instance of user
-                                                                        //body already has email and password keys, so passed directly
-    user.save().then(() => {                                            //Saving the user in the database
+    var user = new User(body); //New instance of user
+    //body already has email and password keys, so passed directly
+    user.save().then(() => { //Saving the user in the database
         return user.generateAuthToken();
     }).then((token) => {
-        res.header('x-auth',token).send('You are registered');                          //token sent back as header
+        res.header('x-auth', token).send('You are registered'); //token sent back as header
     }).catch((err) => {
         console.log(err);
         res.status(400).send('Registration error');
@@ -51,20 +51,24 @@ app.post('/register',(req,res)=>{
 
 })
 
- //Logging in users
- app.post('/login',(req,res)=>{                                    //send email an password in the request
+//Logging in users
+app.post('/login', (req, res) => { //send email an password in the request
 
-    var body=_.pick(req.body,['email','password']);
-    
-    User.findByCredentials(body.email,body.password).then((user) => {   //verifying user with these credentials exists
-        
-        user.generateAuthToken().then((token) => {                      //generate token for user who logged in
-            res.header('x-auth',token).send('You are logged in');                      //send token back in a header
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => { //verifying user with these credentials exists
+
+        user.generateAuthToken().then((token) => { //generate token for user who logged in
+            res.header('x-auth', token).send('You are logged in'); //send token back in a header
         })
     }).catch((err) => {
         console.log(err);
         res.status(400).send('Login error');
-    }); 
+    });
 })
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
